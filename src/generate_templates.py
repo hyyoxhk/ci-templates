efed9d69f04e9c362a807e19cd2ba6a535ee14e8
Benjamin Tissoires <benjamin.tissoires@gmail.com>
2019-11-29 11:24:53 +0100

    generate_template: instead of defining defaults with python use a YAML file
    This allows to simplify the definition of each default, and also
    documents our YAML.
    
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@gmail.com>

--- src/generate_templates.py
+++ src/generate_templates.py
@@ -10,43 +10,53 @@ from pathlib import Path
 if __name__ == '__main__':
     config_data = {}
 
-    # load the various definitions in the provided YAML files
-    src_folder = Path('src')
-    for distrib in [x for x in src_folder.iterdir()
-                    if x.name.endswith('.yml')]:
-        with open(distrib) as fd:
-            data = yaml.load(fd, Loader=yaml.Loader)
-
-            # add some generated variables to keep the template simple
-            for config in data.values():
-                config['DISTRIBUTION'] = config['distribution'].upper()
-                if 'image' in config:
-                    config['docker_repo'] = config['image'].split('/')[0]
-                else:
-                    config['docker_repo'] = config['distribution']
-                if 'version' in config:
-                    config['VERSION'] = config['version']
-                    if 'image' not in config:
-                        config['image'] = f'{config["distribution"]}:{config["version"]}'
-                else:
-                    config['VERSION'] = f'${config["DISTRIBUTION"]}_VERSION'
-                if 'image' not in config:
-                    config['image'] = '$DISTRO:$DISTRO_VERSION'
-                config['PACKAGES'] = f'{config["DISTRIBUTION"]}_{config["package_type"]}'.upper()
-                if 'aarch64' not in config:
-                    config['aarch64'] = True
-            config_data.update(data)
-
-    env = jinja2.Environment(loader = jinja2.FileSystemLoader('./src'),
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader('./src'),
                              trim_blocks=True,
                              lstrip_blocks=True)
 
+    # load the various definitions in the provided YAML files
+    src_folder = Path('src')
+
+    defaults_template = env.get_template('defaults.yml')
+
+    for distrib in [x for x in src_folder.iterdir()
+                    if (x.name.endswith('.yml')
+                        and x.name not in 'defaults.yml')]:
+        with open(distrib) as fd:
+            data = yaml.load(fd, Loader=yaml.Loader)
+
+            # for each root element in each yaml file
+            for config in data.values():
+
+                # add the uppercase version of each key
+                for key, value in [(k, v) for (k, v) in config.items()
+                                   if isinstance(v, str)]:
+                    config[key.upper()] = value.upper()
+
+                # special case I could not figure out with defaults.yml
+                if 'VERSION' in config:
+                    del(config['VERSION'])
+
+                # render the defaults template with the current root element
+                defaults = yaml.load(defaults_template.render(config),
+                                     Loader=yaml.Loader)['defaults']
+
+                # add missing keys from the generated defaults
+                for key, value in defaults.items():
+                    if key not in config:
+                        config[key] = value
+
+            # store the geenrated values in the base config dictionnary
+            config_data.update(data)
+
+    # load our distribution template
     template = env.get_template('template.tmpl')
 
     out_folder = Path('templates')
     if not out_folder.exists():
         os.mkdir(out_folder)
 
+    # and render each distribution in the templates source directory
     for distrib, config in config_data.items():
         dest = out_folder / f'{distrib}.yml'
         print(f'generating {dest}')
